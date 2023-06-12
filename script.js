5afffb9ae7e0f2ffc868ef8d2c57cdbcc3ba968d
Vishal Chavan <103259268+vishalvchavan7@users.noreply.github.com>
2023-06-12 18:27:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ function myFunction() {
   
     var x = document.getElementById("mySearch");
     //var x = value
-    //var defaultVal = x.defaultVal;
-    var currentVal = x;
+    var defaultVal = x.defaultVal;
+    var currentVal = defaultVal;
     console.log(currentVal);
     ABC =  `"https://www.omdbapi.com/?t=${currentVal}&apikey=cf60bf6d"`
     console.log(ABC);
